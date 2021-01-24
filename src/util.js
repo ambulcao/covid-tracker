84b019819e5dc14a2d8b75a4e1fc67c00a91f610
Alexandre Bulcao <ambulcao@gmail.com>
2021-01-24 13:38:35 +0000

    Informaçoes interativos no mapa

--- src/util.js
+++ src/util.js
@@ -1,5 +1,49 @@
+import React from "react";
+import numeral from "numeral";
+import { Circle, Popup } from "react-leaflet";
+
+const casesTypeColors = {
+    cases: {
+        hex: "#CC1034",
+        multiplier: 400,
+    },
+    recovered: {
+        hex: "#7dd71d",
+        multiplier: 800,
+    },
+    deaths: {
+        hex: "fb4443",
+        multiplier: 1600,
+    },
+};
+
 export const sortData = (data) => {
     const sortedData = [...data];
 
     return sortedData.sort((a, b) => (a.cases > b.cases ? -1 : 1));
 };
+
+//Desenhos de círculos no mapa com ferramenta interativa.
+
+export const showDataOnMap = (data, casesType="cases") => 
+    data.map((country) => (
+        <Circle
+            center={[country.countryInfo.lat, country.countryInfo.long]}
+            fillOpacity={0.4}
+            color={casesTypeColors[casesType].hex}
+            fillColor={casesTypeColors[casesType].hex}
+            radius={
+                Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+            }
+        >
+            <Popup>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>    
+            </Popup>
+        </Circle>    
+    ));
